530f9345a3b34d8eed3a7f27767556486bfd1b23
KEILLANI <keillani.kk@gmail.com>
2019-06-07 18:39:23 -0300

    aula_04

---
+++ src/aula_04/ContaCorrente.java
@@ -0,0 +1,18 @@
+package aula_04;
+//Quando uma classe herda de outra ela recebe seus atributos e métodos
+
+public class ContaCorrente extends Conta {
+
+    //chamando o construtor específico passando os parâmetros específicos
+    public ContaCorrente(int agencia, int numero) {
+        super(agencia, numero);
+    }
+
+    //sobrescrita: assinatura deve ser igual
+    //É usada para sobrescrever o método da classe mãe, indicando que o método original foi alterado.
+    @Override
+    public boolean saca(double valor) {
+        double valorASacar = valor + 0.2;
+        return super.saca(valor);
+    }
+}
